b436691d8ed3712035895acef56720c93ea17ecc
Valeriy Nizovtsev <nizucer123@gmail.com>
2022-11-20 18:55:36 +0200

    Update contactsReducer.js

--- src/redux/contactsReducer.js
+++ src/redux/contactsReducer.js
@@ -4,10 +4,10 @@ import { addContact, deleteContact, filterContact } from './actions.js';
 export const contactsReducer = createReducer(
   {
     items: JSON.parse(window.localStorage.getItem('contacts')) ?? [
-      { id: 'id-1', name: 'Vladimir Zelenskiy', number: '044-253-32-45' },
-      { id: 'id-2', name: 'Aleksey Arestovich', number: '044-123-78-96' },
-      { id: 'id-3', name: 'Valeriy Zalugniy', number: '044-897-56-14' },
-      { id: 'id-4', name: 'Igor Terehov', number: '057-344-56-97' },
+      { id: 'id-1', name: 'Vladimir Zelenskiy', number: '253-32-45' },
+      { id: 'id-2', name: 'Aleksey Arestovich', number: '123-78-96' },
+      { id: 'id-3', name: 'Valeriy Zalugniy', number: '897-56-14' },
+      { id: 'id-4', name: 'Igor Terehov', number: '344-56-97' },
     ],
     filter: '',
   },
